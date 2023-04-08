fc90f9ecc79acd606cf36605fde15ae73652a17f
Abhishek Mankar <abhishekmankar11.am@gmail.com>
2023-04-08 21:31:46 +0530

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,124 +1,144 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { nanoid } from "nanoid";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { MdDarkMode, MdDelete, MdLightMode } from "react-icons/md";
 
-const inter = Inter({ subsets: ['latin'] })
+const ThemeContext = createContext(null);
 
-export default function Home() {
+const SearchNotes = ({ handleSearch }) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <input
+      type="text"
+      className="bg-white dark:bg-slate-800 dark:text-white shadow w-96 px-4 py-2 rounded-full text-sm focus:outline-1 focus:outline-indigo-600"
+      placeholder="Search notes..."
+      onChange={handleSearch}
+    />
+  )
+}
+
+const Note = ({ note, handleDeleteNote, ...props }) => {
+  return (
+    <div className={`bg-white dark:bg-slate-800 dark:text-white shadow w-full h-52 rounded flex flex-col ${props.className}`}>
+      <div className="flex-grow whitespace-pre-wrap text-sm p-4">
+        {note.text}
+      </div>
+      <div className="flex justify-between items-center px-4 py-2">
+        <div className="text-xs text-gray-600 dark:text-gray-400 rounded-b">
+          {note.date}
+        </div>
+        <div className="">
+          <button className="text-gray-400 hover:text-gray-800 dark:hover:text-white" onClick={() => handleDeleteNote(note.id)}><MdDelete /></button>
         </div>
       </div>
+    </div>
+  )
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+const NewNote = ({ handleAddNote, ...props }) => {
+  const [note, setNote] = useState("");
+
+  return (
+    <div className={`bg-white dark:bg-slate-800 dark:text-white shadow w-full h-52 rounded flex flex-col ring-2 ring-transparent focus-within:ring-indigo-600 ${props.className}`}>
+      <textarea
+        className="dark:bg-slate-800 p-4 w-full h-full resize-none outline-none text-sm overflow-auto rounded-t"
+        value={note}
+        placeholder="Type here something..."
+        onChange={(e) => setNote(e.target.value)}
+      // onKeyDown={(e) => {
+      //   if (e.key == "Enter") {
+      //     handleAddNote(note);
+      //     setNote("");
+      //   }
+      // }}
+      ></textarea>
+      <div className="flex justify-between items-center px-4 py-2">
+        <div className="text-xs text-gray-600 dark:text-gray-400 rounded-b">
+          {new Date().toDateString()}
+        </div>
+        <div className="">
+          <button
+            className="bg-gray-200 dark:bg-indigo-600 dark:text-white text-xs px-4 py-2 rounded-full text-gray-800 hover:bg-indigo-600 hover:text-white transition-colors"
+            onClick={() => {
+              handleAddNote(note);
+              setNote("");
+            }}
+          >Save</button>
+        </div>
       </div>
+    </div>
+  )
+}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+const ThemeSwitcher = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  useEffect(() => {
+    document.documentElement.classList.remove("light");
+    document.documentElement.classList.remove("dark");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+  return (
+    <button
+      className="absolute bg-white dark:bg-slate-800 dark:text-white flex justify-center items-center shadow w-8 h-8 rounded-full top-4 right-4"
+      onClick={() => setTheme(prevState => (prevState === "light") ? "dark" : "light")}
+    >
+      {theme === "light" ? <MdDarkMode /> : <MdLightMode />}
+    </button>
+  )
+}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+export default function Home() {
+  const [notes, setNotes] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const firstRender = useRef(true);
+
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("notes"));
+    if (savedNotes) setNotes(savedNotes);
+  }, []);
+
+  useEffect(() => {
+    if (firstRender.current) firstRender.current = false;
+    else localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes, firstRender]);
+
+  const handleAddNote = (note) => {
+    setNotes(prevState => ([
+      ...prevState,
+      {
+        id: nanoid(),
+        text: note,
+        date: new Date().toDateString()
+      }
+    ]))
+  }
+
+  const handleDeleteNote = (id) => {
+    const updatedNotes = notes.filter(item => item.id !== id);
+    setNotes(updatedNotes);
+  }
+
+  const handleSearch = (e) => setSearchText(e.target.value.toLocaleLowerCase());
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <main className="relative bg-slate-200 w-full min-h-screen pb-10 dark:bg-slate-900">
+        <ThemeSwitcher />
+        <div className="max-w-6xl m-auto">
+          <div className="flex justify-center p-4 mb-6">
+            <SearchNotes handleSearch={handleSearch} />
+          </div>
+          <div className="grid grid-cols-3 gap-4">
+            {notes.filter((note) => note.text.toLowerCase().includes(searchText)).map((note, index) => (
+              <Note key={index} className="col-span-1" note={note} handleDeleteNote={handleDeleteNote} />
+            ))}
+            <NewNote handleAddNote={handleAddNote} />
+          </div>
+        </div>
+      </main>
+    </ThemeContext.Provider>
   )
 }
